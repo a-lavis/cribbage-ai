d05cb19224a946d0d041819735a65dec8c259ad5
Daniel Melody <dmwalter12@gmail.com>
2019-04-14 10:49:25 -0400

    contracts for most scoring functions written

--- scoring.lisp
+++ scoring.lisp
@@ -15,15 +15,84 @@
 ;; ------------------------------------------
 ;; INPUTS: DEALER-SCORE, the representation of a dealer's SCORE
 ;;         CUT, the card flipped over after the CUT
-;; OUTPUTS: the updated dealer's score value
+;; OUTPUTS: the updated dealer's current score value (adds 2)
+;; CONDITION: when the card placed on top of the deck is a JACK of any SUIT
 
 (defun his-heels (dealer-score cut)
   ;; get dealer's current SCORE
-  (let ((curr-score (second dealer-score))))
+  (let ((dealer-curr (second dealer-score)))
     ;; if CUT == any JACK
     (when (or (equal cut (* *jack* *spades*))
               (equal cut (* *jack* *clubs*))
               (equal cut (* *jack* *hearts*))
               (equal cut (* *jack* *diamonds*)))
-      ;; add 1 to DEALER-SCORE
-      (incf dealer-score)))
+      ;; add 2 to DEALER-CURR
+      (+ dealer-curr 2))))
+
+
+;; HIS-KNOBS
+;; ------------------------------------------
+;; INPUTS: TOP-CARD, the top card of the PILE
+;;         CUT-SUIT, the SUIT of the card placed after the CUT
+;;         LAST-PLAYER, the last player to place a CARD
+;; OUTPUTS: the updated LAST-PLAYER's score (adds 1)
+;; CONDITION: the TOP-CARD matches the CUT-SUIT
+
+
+;; GO
+;; ------------------------------------------
+;; INPUTS: PILE-SUM, the sum of the cards placed in the PILE
+;;         LAST-PLAYER, the last player to place a CARD
+;; OUTPUTS: the updated LAST-PLAYER's score (adds 1)
+;; CONDITION: when the next player can't play a card because the PILE-SUM
+;;    is already 31 or would go over
+
+
+;; FIFTEEN
+;; ------------------------------------------
+;; INPUTS: PILE-SUM, the sum of the cards placed in the PILE
+;;         LAST-PLAYER, the last player to place a CARD
+;; OUTPUTS: the updated LAST-PLAYER's score (adds 2)
+;; CONDITION: when the PILE-SUM reaches 15
+
+
+;; PAIR
+;; ------------------------------------------
+;; INPUTS: CARD-PILE, the pile of cards
+;;         LAST-PLAYER, the last player to place a CARD
+;; OUTPUTS: the updated LAST-PLAYER's score (adds 2)
+;; CONDITION: when the previous two cards make a PAIR
+
+
+;; TRIPLE
+;; ------------------------------------------
+;; INPUTS: CARD-PILE, the pile of cards
+;;         LAST-PLAYER, the last player to place a CARD
+;; OUTPUTS: the updated LAST-PLAYER's score (adds 6)
+;; CONDITION: when the previous three cards make a TRIPLE
+
+
+;; QUADRUPLE
+;; ------------------------------------------
+;; INPUTS: CARD-PILE, the pile of cards
+;;         LAST-PLAYER, the last player to place a CARD
+;; OUTPUTS: the updated LAST-PLAYER's score (adds 12)
+;; CONDITION: when the previous three cards make a QUADRUPLE
+
+
+;; RUN
+;; ------------------------------------------
+;; INPUTS: CARD-PILE, the pile of cards
+;;         NUM-CARDS, the number of cards in the RUN
+;;         LAST-PLAYER, the last player to place a CARD
+;; OUTPUTS: the updated LAST-PLAYER's score (adds NUM-CARDS)
+;; CONDITION: the last NUM-CARD's make have continuous
+;;    rank (can be out of order)
+
+
+;; FLUSH -- might not be in the pile    ************************************
+;; ------------------------------------------
+;; INPUTS: CARD-PILE, the pile of cards
+;;         LAST-PLAYER, the last player to place a CARD
+;; OUTPUTS: the updated LAST-PLAYER's score (adds 5)
+;; CONDITION: the last 5 cards have the same SUIT
