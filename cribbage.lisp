f8c45e46d29c3cc0ed22419cfe40c3a903bdfa5b
Daniel Melody <dmwalter12@gmail.com>
2019-04-24 17:02:11 -0400

    started working on generate-cards function

--- cribbage.lisp
+++ cribbage.lisp
@@ -45,7 +45,7 @@
 ;; OUTPUTS: none
 ;; SIDE-EFFECTS: changes whose turn it is
 
-(defun toggle-turn! (c)
+(defun toggle-dealer! (c)
   ;; get the current dealer
   (let ((curr-dealer (cribbage-whose-dealer? c)))
     (setf (cribbage-whose-dealer? c) (switch curr-dealer))))
@@ -66,14 +66,35 @@
 ;;  OUTPUTS:
 
 (defun play-round (c)
-  (while (not (game-over? c))
-    (deal c)
-    (play c)
-    (show c)))
+    (while (not (game-over? c))
+      (deal c)
+      (play c)
+      (show c)))
 
 
-;; DEAL
+;; GENERATE-CARDS
 ;; ------------------------------------------
+;; INPUTS: none
+;; OUTPUTS: a list of two lists of cards (each player's hand)
+
+(defun generate-cards ()
+  ;; accumulator list for cards dealt
+  (labels ((acc-func (card-bucket)
+              ;; deal a CARD
+              (let ((card (deal-card)))
+                (cond
+                  ;;  (LENGTH CARD-BUCKET) == 11
+                  ((>= (length card-bucket) 11)
+                    card-bucket)
+                  ;; CARD is not a MEMBER of CARD-BUCKET
+                  ((not (member card card-bucket))
+                    (acc-func (cons card card-bucket)))
+                  ;; CARD is a MEMBER of CARD-BUCKET
+                  (#t
+                    (acc-func card-bucket))))))
+      ;; call ACC-FUNC w/ empty CARD-BUCKET
+      (acc-func '())))
+
 
 ;; PLAY
 ;; ------------------------------------------
