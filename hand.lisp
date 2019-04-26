e9440dc2ef81f5e8b1821352181df0f1a2b1d396
a-lavis <alavis@vassar.edu>
2019-04-26 17:34:36 -0400

    hand values

--- hand.lisp
+++ hand.lisp
@@ -2,33 +2,49 @@
   (list c1 c2 c3))
 
 (defun hand-value (cut hand)
-  (+ (fifteen-score cut hand)
-     (tuple-score cut hand)
-     (run-score cut hand)
-     (flush-score cut hand)))A
+  (let ((cards (cons cut hand)))
+    (+ (fifteen-score cards)
+       (tuple-score cards)
+       (run-score cards)
+       (flush-score cards))))
 
 (defun power-set (listy)
   (if (null listy)
     listy
     (let ((rest-pset (power-set (rest listy))))
-      (append (map (lambda (subby)
+      (append (mapcar (lambda (subby)
                      (cons (first listy) subby))
                    rest-pset)
               rest-pset))))
 
-(defun fifteen-score (cut hand)
+(defun fifteen-score (cards)
   (labels ((count-fifteens (pset)
                            (cond ((null pset) 0)
                                  ((= (apply #'+ hand) 15)
                                   (+ 1 (count-fifteens (rest pset))))
                                  (else (count-fifteens (rest pset))))))
-    (count-fifteens (power-set (cons cut hand)))))
+    (count-fifteens (power-set (mapcar #'card-value cards)))))
 
-(defun tuple-score (cut hand)
-  (labels ((count-tuples (ordered counter current)
-                         (cond ((null ordered) 0)
-                               ((= (first ordered) current)
-                                (
+#|
+(defun fifteen-score (cards)
+  (labels ((count-fifteens
+             (c-vals)
+             (if (null c-vals)
+               0
+               (let ((rest-15s (count-15s (rest c-vals))))
+                 (+ ())))))))
+|#
 
-  (count-tuples (sort (cons cut hand) #'<)))
-
+#|  WORK IN PROGRESS
+(defun tuple-score (cards)
+  (labels ((count-tuples
+             (ranks counter current-rank)
+             (cond ((null ranks) 0)
+                   ((= (first ranks) current-rank)
+                    (count-tuples (rest ordered) (+ 1 counter) current-rank))
+                   ((= counter 2)
+                    (+ 2 (count-tuples (rest ordered) 0 )))
+                   )))
+    (let ((ranks (sort (mapcar #'rank-of cards) #'<)))
+      (count-tuples (rest ranks) 1 (first ranks)))))
+|#
