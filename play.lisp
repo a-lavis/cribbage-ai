cbea80936a1a22777e9be6b1186260bc713dafdd
Daniel Melody <dmwalter12@gmail.com>
2019-05-03 15:58:08 -0400

    using incf instead of setf in hand-to-pile!

--- play.lisp
+++ play.lisp
@@ -14,26 +14,24 @@
 ;; SIDE-EFFECTS: removes CARD from player's hand
 
 (defun hand-to-pile! (c check-legal? card plr)
-;; get player's hand
-(let ((plr-hand (svref (cribbage-plr-hands c) plr)))
-  ;; check if CARD is legal (ie. in hand)... if CHECK-LEGAL? == T
-  (when (and check-legal? (not (legal-play? plr-hand (cribbage-pile c) card)))
-    ;; print error message
-    (format t "Illegal play! Potential issues: pile will go over 31 with
-      any of your remaining cards, no cards left, not a card.")
-    (return-from hand-to-pile! nil))
-  ;; remove CARD from PLR-HAND
-  (setf plr-hand (remove card plr-hand))
-  ;; add CARD to PILE
-  (setf (cribbage-pile c) (cons card (cribbage-pile c)))
-  ;; update CRIBBAGE-PLR-HANDS
-  (setf (svref (cribbage-plr-hands c) plr) plr-hand)
-  ;; call PILE-SCORE
-  (setf (svref (cribbage-score c) plr)
-  ( + (svref (cribbage-score c) plr)
-  (pile-score c)))
-  ;; change WHOSE-TURN?
-  (setf (cribbage-whose-turn? c) (toggle-turn! c)))
+  ;; get player's hand
+  (let ((plr-hand (svref (cribbage-plr-hands c) plr)))
+    ;; check if CARD is legal (ie. in hand)... if CHECK-LEGAL? == T
+    (when (and check-legal? (not (legal-play? plr-hand (cribbage-pile c) card)))
+      ;; print error message
+      (format t "Illegal play! Potential issues: pile will go over 31 with
+        any of your remaining cards, no cards left, not a card.")
+      (return-from hand-to-pile! nil))
+    ;; remove CARD from PLR-HAND
+    (setf plr-hand (remove card plr-hand))
+    ;; add CARD to PILE
+    (setf (cribbage-pile c) (cons card (cribbage-pile c)))
+    ;; update CRIBBAGE-PLR-HANDS
+    (setf (svref (cribbage-plr-hands c) plr) plr-hand)
+    ;; call PILE-SCORE
+    (incf (svref (cribbage-score c) plr) (pile-score c))
+    ;; change WHOSE-TURN?
+    (toggle-turn! c))
   ;; print Cribbage struct
   (print-cribbage c t 1))
 
