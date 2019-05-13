4b898a671a5918ba260155cee2b9eb4adc38a1e0
Daniel Melody <dmwalter12@gmail.com>
2019-05-13 11:02:15 -0400

    play-round and play-game structured for 2-plr game w/ potentially different strategies

--- game.lisp
+++ game.lisp
@@ -7,42 +7,54 @@
 ;; PLAY-ROUND
 ;; ------------------------------------------
 ;; INPUTS: C, a Cribbage game
-;;         TO-CRIB, a fxn for deciding cards given to CRIB
-;;         TO-PILE, a fxn for deciding cards given to PILE
+;;         P1-CRIB-FN, a fxn for deciding cards given to CRIB for player 1
+;;         P1-PILE-FN, a fxn for deciding cards given to PILE for player 1
+;;         P2-CRIB-FN, a fxn for deciding cards given to CRIB for player 2
+;;         P2-PILE-FN, a fxn for deciding cards given to PILE for player 2
 ;; OUTPUTS: none
 ;; SIDE EFFECTS: plays a round of Cribbage and updates Cribbage struct
 
-(defun play-round (c crib-fn pile-fn)
+(defun play-round (c p1-crib-fn p1-pile-fn p2-crib-fn p2-pile-fn)
   (cond
     ((not (game-over? c))
       (let ((p1-hand (svref (cribbage-plr-hands c) 0))
             (p2-hand (svref (cribbage-plr-hands c) 1))
-            (pile (cribbage-pile c)))
+            (dlr (cribbage-whose-dealer? c))
+	    (pile (cribbage-pile c)))
+	;; DEAL the cards
+	(deal c)
         ;; DEALER's hand-to-crib!
-        (funcall crib-fn c)
+        (if (equal dlr *player-one*)
+	    (funcall p1-crib-fn c)
+	  (funcall p2-crib-fn c))
         ;; NON-DEALER's hand-to-crib!
-        (funcall crib-fn c)
+        (if (equal dlr *player-one*)
+	    (funcall p2-crib-fn c)
+	  (funcall p1-crib-fn c))
         ;; call hand-to-pile! as many times as necessary/possible
         (while (or (dolist (card p1-hand)
 		     (legal-play? p1-hand pile card))
 		   (dolist (card p2-hand)
 		     (legal-play? p2-hand pile card)))
-          (funcall pile-fn c))))
+          (if (equal (cribbage-whose-turn? c) *player-one*)
+	      (funcall p1-pile-fn c)
+	    (funcall p2-pile-fn c)))))
     (t
       (format t "Unable to play a round.~%"))))
 
 
 ;; PLAY-GAME
 ;; ------------------------------------------
-;; INPUTS: C, a Cribbage game
-;;         CRIB-FN, a function governing hand-to-crib! choices
-;;         PILE-FN, a function governing hand-to-pile! choices
-;; OUTPUTS: none
-;; SIDE EFFECTS: plays a game of Cribbage
+;; INPUTS: P1-CRIB-FN, a function governing hand-to-crib! choices for player 1
+;;         P1-PILE-FN, a function governing hand-to-pile! choices for player 1
+;;         P2-CRIB-FN, a function governing hand-to-crib! choices for player 2
+;;         P2-PILE-FN, a function governing hand-to-pile! choices for player 2
+;; OUTPUTS: the winning player
 
-(defun play-game (c crib-fn pile-fn)
-  ;; PLAY-ROUND while NOT GAME-OVER?
-  (while (not (game-over? c))
-    (play-round c crib-fn pile-fn))
-  ;; return who won
-  (format t "Congratulations, Player ~A~%" (who-won? c)))
+(defun play-game (p1-crib-fn p1-pile-fn p2-crib-fn p2-pile-fn)
+  (let ((c (make-cribbage)))
+    ;; PLAY-ROUND while NOT GAME-OVER?
+    (while (not (game-over? c))
+      (play-round c p1-crib-fn p1-pile-fn p2-crib-fn p2-pile-fn))
+    ;; return who won this game
+    (who-won? c)))
