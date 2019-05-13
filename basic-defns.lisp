b41f1f680bb2cb23e45fbee99ebfb920f1f5dd54
Daniel Melody <dmwalter12@gmail.com>
2019-05-13 10:03:08 -0400

    using funcall now in play-round, still not working tho

--- basic-defns.lisp
+++ basic-defns.lisp
@@ -17,6 +17,7 @@
         "deal"
         "play"
         "show"
+	"game"
         ))
 
 
--- game.lisp
+++ game.lisp
@@ -10,19 +10,21 @@
 ;;         TO-CRIB, a fxn for deciding cards given to CRIB
 ;;         TO-PILE, a fxn for deciding cards given to PILE
 
-(defun play-round (c to-crib to-pile)
+(defun play-round (c crib-fn pile-fn)
   (cond
     ((not (game-over? c))
       (let ((p1-hand (svref (cribbage-plr-hands c) 0))
             (p2-hand (svref (cribbage-plr-hands c) 1))
             (pile (cribbage-pile c)))
         ;; DEALER's hand-to-crib!
-        (apply #'to-crib c)
+        (funcall crib-fn c)
         ;; NON-DEALER's hand-to-crib!
-        (apply #'to-crib c)
+        (funcall crib-fn c)
         ;; call hand-to-pile! as many times as necessary/possible
-        (while (or (legal-pile? p1-hand *CARD*)
-                  (legal-pile! p2-hand #CARD*))
-          (apply #'to-pile c))))
+        (while (or (dolist (card p1-hand)
+		     (legal-play? p1-hand pile card))
+		   (dolist (card p2-hand)
+		     (legal-play? p2-hand pile card)))
+          (funcall pile-fn c))))
     (t
       (format t "Unable to play a round.~%"))))
