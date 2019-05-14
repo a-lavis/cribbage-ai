384869d4f975266c01cae78e1a3741745992b84e
a-lavis <alavis@vassar.edu>
2019-05-14 17:00:53 -0400

    avg function

--- game.lisp
+++ game.lisp
@@ -80,6 +80,63 @@
     ;; return who won this game
     (who-won? c)))
 
+;; PLAY-ONLY-GAME
+
+(defun play-only-game (p1-crib-fn p1-pile-fn p2-crib-fn p2-pile-fn)
+  (let ((c (make-cribbage)))
+    ;; PLAY-ROUND while NOT GAME-OVER?
+    (while (not (game-over? c))
+      (play-only-round c p1-crib-fn p1-pile-fn p2-crib-fn p2-pile-fn))
+    ;; return who won this game
+    (who-won? c)))
+
+;; PLAY-ONLY-ROUND
+
+(defun play-only-round (c p1-crib-fn p1-pile-fn p2-crib-fn p2-pile-fn)
+  (cond
+    ((not (game-over? c))
+     (let ((dlr (cribbage-whose-dealer? c))
+           (card nil)
+           (last-card nil))
+       ;; set turn to DLR
+       (setf (cribbage-whose-turn? c) dlr)
+       ;; DEAL the cards
+       (format t "DEAL~%")
+       (deal c)
+       ;; give cards to CRIB
+       (cond
+         ;; DLR = Player One
+         ((equal dlr *player-one*)
+          (format t "DLR to crib.~%")
+          (funcall p1-crib-fn c)
+          (format t "NON-DLR to crib.~%")
+          (funcall p2-crib-fn c)
+          ;; set first CARD selection to P2-PILE-FN
+          (setf card (funcall p2-pile-fn c)))
+         ((equal dlr *player-two*)
+          (format t "DLR to crib.~%")
+          (funcall p2-crib-fn c)
+          (format t "NON-DLR to crib.~%")
+          (funcall p1-crib-fn c)
+          ;; set first CARD selection to P1-PILE-FN
+          (setf card (funcall p1-pile-fn c))))
+
+       ;; the PLAY, call hand-to-pile! as many times as necessary/possible
+       (while (or card last-card)
+              (setf last-card card)
+              (cond
+                ;; call P1-PILE-FN
+                ((= (cribbage-whose-turn? c) *player-one*)
+                 (setf card (funcall p1-pile-fn c)))
+                ;; call P2-PILE-FN
+                ((= (cribbage-whose-turn? c) *player-two*)
+                 (setf card (funcall p2-pile-fn c)))
+                ;; default, assign CARD to NIL to ward off infinite loop
+                (t
+                  (setf card nil))))
+       ))
+    (t
+      (format t "Unable to play a round.~%"))))
 
 ;; RANDOM-ROUND
 ;; ------------------------------------------
@@ -138,3 +195,32 @@
 (defun pi-vs-random ()
   (play-game #'random-to-crib! #'pi-mcts-to-pile!
              #'random-to-crib! #'random-to-pile!))
+
+;; AVG-GAME-WINS
+
+(defun avg-game-wins (times)
+  (let ((r (avg-random-wins times))
+        (p1 (avg-mcts-p1-wins times))
+        (p2 (avg-mcts-p2-wins times)))
+    (format t "random: ~A/50~%p1: ~A/50~%p2: ~A/50~%" r p1 p2)))
+
+(defun avg-random-wins (times)
+  (let ((x 0))
+    (dotimes (i times)
+      (incf x (play-only-game #'random-to-crib! #'random-to-pile!
+                              #'random-to-crib! #'random-to-pile!)))
+    (- x times)))
+
+(defun avg-mcts-p1-wins (times)
+  (let ((x 0))
+    (dotimes (i times)
+      (incf x (play-only-game #'random-to-crib! #'pi-mcts-to-pile!
+                              #'random-to-crib! #'random-to-pile!)))
+    (- times (- x times))))
+
+(defun avg-mcts-p2-wins (times)
+  (let ((x 0))
+    (dotimes (i times)
+      (incf x (play-only-game #'random-to-crib! #'random-to-pile!
+                              #'random-to-crib! #'pi-mcts-to-pile!)))
+    (- x times)))
